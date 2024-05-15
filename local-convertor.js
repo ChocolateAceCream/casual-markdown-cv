a5d8c9454cdbe7ea7dbea3f1750245588e08a762
ChocolateAceCream <nuodi@hotmail.com>
2024-05-15 16:13:53 -0700

    add more versions

---
+++ local-convertor.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+
+async function generatePDF() {
+    // Launch a new browser session.
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+    // Open a new page.
+    const page = await browser.newPage();
+    // Navigate to the URL.
+  await page.goto('http://127.0.0.1:5500/resume-web.html', { waitUntil: 'networkidle0' });
+    // Generate PDF and save it.
+    await page.pdf({
+        path: 'resume.pdf', // The file path to save the PDF to.
+        format: 'A4',       // Specify the format of the PDF.
+        printBackground: true // Ensures that the background styles are also printed.
+    });
+    // Close the browser session.
+    await browser.close();
+    console.log('PDF Generated!');
+}
+
+generatePDF();
