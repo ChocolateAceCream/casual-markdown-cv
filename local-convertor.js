5c3d9e5eba36bd9d67edda83937139a190db34df
ChocolateAceCream <nuodi@hotmail.com>
2024-05-21 19:44:43 -0700

    add date

--- local-convertor.js
+++ local-convertor.js
@@ -9,15 +9,21 @@ async function generatePDF() {
     const page = await browser.newPage();
     // Navigate to the URL.
   await page.goto('http://127.0.0.1:5500/resume-web.html', { waitUntil: 'networkidle0' });
-    // Generate PDF and save it.
-    await page.pdf({
-        path: 'resume.pdf', // The file path to save the PDF to.
-        format: 'A4',       // Specify the format of the PDF.
-        printBackground: true // Ensures that the background styles are also printed.
-    });
-    // Close the browser session.
-    await browser.close();
-    console.log('PDF Generated!');
+  // Generate PDF and save it.
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Months are 0-based, so add 1
+  const day = String(currentDate.getDate()).padStart(2, '0');
+
+  const filename = `resume-${year}-${month}-${day}.pdf`;
+  await page.pdf({
+    path: filename, // The file path to save the PDF to.
+    format: 'A4',       // Specify the format of the PDF.
+    printBackground: true // Ensures that the background styles are also printed.
+  });
+  // Close the browser session.
+  await browser.close();
+  console.log('PDF Generated!');
 }
 
 generatePDF();
